7186ccec2fa7e0e8de8f9907f599c0a6219f7f37
teru12012000 <sion2769@gmail.com>
2024-04-20 23:06:14 +0900

    feat: bun api

--- bun-varify/index.ts
+++ bun-varify/index.ts
@@ -1,10 +1,13 @@
-import express, { type Request, type Response } from "express";
+const server = Bun.serve({
+  port: 6060,
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response("Bun!");
+    else if (url.pathname === "/getTask") return new Response("hogehoge");
+    else if (url.pathname === "/post") return new Response("hogehoge");
 
-const app = express();
-
-app.use(express.json());
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello");
+    return new Response("404");
+  },
 });
 
-app.listen(8080);
+console.log(`Listening on http://localhost:${server.port} ...`);
